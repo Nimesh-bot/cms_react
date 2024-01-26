ec050ec75bb620125035f0bdbcd23f7e92dd1358
Nimesh-11 <57608111+Nimesh-bot@users.noreply.github.com>
2024-01-26 20:23:08 +0545

    chore: sticky menu

---
+++ src/layout/HeaderMenu.tsx
@@ -0,0 +1,11 @@
+const HeaderMenu = () => {
+  return (
+    <div className="header-menu sticky top-0 left-0">
+      <ul>
+        <li>Light</li>
+      </ul>
+    </div>
+  );
+};
+
+export default HeaderMenu;
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
+import HeaderMenu from "./HeaderMenu";
 
 import VerticalNavbar from "./VerticalNavbar";
 
 const MainLayout = () => {
   return (
-    <main className="flex gap-x-8 h-screen bg-light dark:bg-dark">
+    <main className="flex h-screen bg-light dark:bg-dark">
       <VerticalNavbar />
 
-      <section className="flex-1 h-full overflow-scroll">
-        <Outlet />
+      <section className="flex-1 h-full overflow-auto relative">
+        <HeaderMenu />
+        <div className="wrapper">
+          <Outlet />
+        </div>
       </section>
     </main>
   );
