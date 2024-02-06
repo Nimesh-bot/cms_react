f68786e75ddb6887f06a6505f1e5429856910325
Nimesh-11 <57608111+Nimesh-bot@users.noreply.github.com>
2024-02-06 22:05:08 +0545

    refactor: vertical navbar to allow n no. of child

--- src/layout/VerticalNavbar.tsx
+++ src/layout/VerticalNavbar.tsx
@@ -8,26 +8,18 @@ const VerticalNavbar = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    type GroupedItems = Record<string, MenuItem[]>;
-
-    const separateByGroup = (items: MenuItem[]): GroupedItems => {
-      return items.reduce((acc: GroupedItems, item: MenuItem) => {
-        const { children, group, ...rest } = item; // Remove children from the item
+    const separateByGroup = (items: MenuItem[]) => {
+      return items.reduce((acc: Record<string, MenuItem[]>, item: MenuItem) => {
+        const { children, group, ...rest } = item;
         if (!acc[group]) {
           acc[group] = [];
         }
-        acc[group].push({
-          ...rest,
-          group: group,
-          children: children,
-        });
-
+        acc[group].push({ ...rest, group, children });
         return acc;
-      }, {} as GroupedItems);
+      }, {} as Record<string, MenuItem[]>);
     };
 
     const separatedItems = separateByGroup(menu);
-
     setMenuItems(separatedItems);
   }, []);
 
@@ -35,6 +27,36 @@ const VerticalNavbar = () => {
     if (path) {
       navigate(path);
     }
+  };
+
+  const renderMenuItems = (items: MenuItem[], marginLeft = 0) => {
+    return items.map((item: MenuItem, index: number) => (
+      <div key={index} className="flex justify-between w-full items-center">
+        {item.children ? (
+          <MenuAccordian
+            element={
+              <div
+                className="flex gap-x-2 items-center"
+                style={{ marginLeft: `${marginLeft}px` }}
+              >
+                {item.icon && <item.icon className="w-5 h-5" />}
+                {item.title}
+              </div>
+            }
+            items={renderMenuItems(item.children, marginLeft + 10)}
+          />
+        ) : (
+          <div
+            className="flex gap-x-2 items-center cursor-pointer"
+            onClick={() => navigationHandler(item.path)}
+            style={{ marginLeft: `${marginLeft}px` }}
+          >
+            {item.icon && <item.icon className="w-5 h-5" />}
+            {item.title}
+          </div>
+        )}
+      </div>
+    ));
   };
 
   return (
@@ -49,75 +71,7 @@ const VerticalNavbar = () => {
             </div>
           )}
           <div key={index} className="flex flex-col gap-y-4">
-            {menuItems[group].map((item: MenuItem, index: number) => (
-              <div
-                key={index}
-                className="flex justify-between w-full items-center"
-              >
-                {item.children ? (
-                  <MenuAccordian
-                    element={
-                      <div className="flex gap-x-2 items-center">
-                        {item.icon && <item.icon className="w-5 h-5" />}
-                        {item.title}
-                      </div>
-                    }
-                    items={item.children.map(
-                      (child: MenuItem, index: number) => (
-                        <div className="ml-sm">
-                          {child.children ? (
-                            <MenuAccordian
-                              key={index}
-                              element={
-                                <div className="flex gap-x-2 items-center">
-                                  {child.icon && (
-                                    <child.icon className="w-5 h-5" />
-                                  )}
-                                  {child.title}
-                                </div>
-                              }
-                              items={child.children.map(
-                                (subChild: MenuItem, index: number) => (
-                                  <div
-                                    key={index}
-                                    className="flex gap-x-2 flex-1 items-center ml-sm cursor-pointer"
-                                    onClick={() =>
-                                      navigationHandler(subChild.path)
-                                    }
-                                  >
-                                    {subChild.icon && (
-                                      <subChild.icon className="w-5 h-5" />
-                                    )}
-                                    {subChild.title}
-                                  </div>
-                                )
-                              )}
-                            />
-                          ) : (
-                            <div
-                              key={index}
-                              className="flex gap-x-2 items-center cursor-pointer"
-                              onClick={() => navigationHandler(child.path)}
-                            >
-                              {child.icon && <child.icon className="w-5 h-5" />}
-                              {child.title}
-                            </div>
-                          )}
-                        </div>
-                      )
-                    )}
-                  />
-                ) : (
-                  <div
-                    className="flex gap-x-2 items-center cursor-pointer"
-                    onClick={() => navigationHandler(item.path)}
-                  >
-                    {item.icon && <item.icon className="w-5 h-5" />}
-                    {item.title}
-                  </div>
-                )}
-              </div>
-            ))}
+            {renderMenuItems(menuItems[group])}
           </div>
         </div>
       ))}
